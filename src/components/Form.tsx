8666530fccdd71337b98166d962c8b53a90f37d7
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-04 14:01:15 -0300

    feat: validate form fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,81 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 function Form() {
   const [showForm, setShowForm] = useState(false);
+  const [inputs, setInputs] = useState({
+    service: '',
+    login: '',
+    password: '',
+    url: '',
+  });
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInputs((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const formIsValid = () => {
+    const { service, login, password } = inputs;
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#$%^&+=!])(?!.*\s).{8,16}$/;
+    const isValid = (
+      service.length > 0
+      && login.length > 0
+      && passwordRegex.test(password)
+    );
+    return isValid;
+  };
 
   return (
     showForm ? (
       <form>
         <label htmlFor="service">
           Nome do Serviço
-          <input type="text" id="service" />
+          <input
+            type="text"
+            id="service"
+            name="service"
+            value={ inputs.service }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
         <label htmlFor="login">
           Login
-          <input type="text" id="login" />
+          <input
+            type="text"
+            id="login"
+            name="login"
+            value={ inputs.login }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
         <label htmlFor="password">
           Senha
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={ inputs.password }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
         <label htmlFor="url">
           URL
-          <input type="text" id="url" />
+          <input
+            type="text"
+            id="url"
+            name="url"
+            value={ inputs.url }
+            onChange={ (e) => handleChange(e) }
+          />
         </label>
-        <button type="button">
+        <button
+          type="button"
+          onClick={ () => {} }
+          disabled={ !formIsValid() }
+        >
           Cadastrar
         </button>
         <button
