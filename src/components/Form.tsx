72dee212d62b157130dae61e45e4d59c5d71ac35
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-04 13:39:59 -0300

    feat: render form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <form>
-      <label htmlFor="service">
-        Nome do Serviço
-        <input type="text" id="service" />
-      </label>
-      <label htmlFor="login">
-        Login
-        <input type="text" id="login" />
-      </label>
-      <label htmlFor="password">
-        Senha
-        <input type="password" id="password" />
-      </label>
-      <label htmlFor="url">
-        URL
-        <input type="text" id="url" />
-      </label>
-      <button type="button">
-        Cadastrar
+    showForm ? (
+      <form>
+        <label htmlFor="service">
+          Nome do Serviço
+          <input type="text" id="service" />
+        </label>
+        <label htmlFor="login">
+          Login
+          <input type="text" id="login" />
+        </label>
+        <label htmlFor="password">
+          Senha
+          <input type="password" id="password" />
+        </label>
+        <label htmlFor="url">
+          URL
+          <input type="text" id="url" />
+        </label>
+        <button type="button">
+          Cadastrar
+        </button>
+        <button
+          type="button"
+          onClick={ () => setShowForm(false) }
+        >
+          Cancelar
+        </button>
+      </form>
+    ) : (
+      <button
+        type="button"
+        onClick={ () => setShowForm(true) }
+      >
+        Cadastrar nova senha
       </button>
-      <button type="button">
-        Cancelar
-      </button>
-    </form>
+    )
   );
 }
 
